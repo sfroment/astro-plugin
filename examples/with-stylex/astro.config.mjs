8886b63a2f05d0c958fb21684452bc36dd97d910
Sacha Froment <sfroment42@gmail.com>
2023-12-10 15:47:00 +0100

    chore: upgrade
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- examples/with-stylex/astro.config.mjs
+++ examples/with-stylex/astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import stylex from "@sfroment/astro-stylex";
 import mdx from "@astrojs/mdx";
+import path from "path";
+
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), stylex()],
+	integrations: [mdx(), stylex({ astroDir: __dirname + "/src" })],
 });
--- packages/stylex-rollup/src/index.ts
+++ packages/stylex-rollup/src/index.ts
@@ -2,16 +2,16 @@
 import stylexPlugin from "@stylexjs/rollup-plugin";
 
 const IS_DEV_ENV =
-  process.env.NODE_ENV === 'development' ||
-  process.env.BABEL_ENV === 'development';
+	process.env.NODE_ENV === "development" ||
+	process.env.BABEL_ENV === "development";
 
 export default ({
 	dev = IS_DEV_ENV,
-  fileName = 'stylex.css',
-	astroDir = '',
-  babelConfig: { plugins = [], presets = [] } = {},
-  ...options
-})  => {
+	fileName = "stylex.css",
+	astroDir = "",
+	babelConfig: { plugins = [], presets = [] } = {},
+	...options
+}) => {
 	const plugin = stylexPlugin({
 		dev,
 		fileName,
@@ -23,12 +23,12 @@ export default ({
 	});
 	return {
 		...plugin,
-		async transform(inputCode = '', id = '') {
-			if (astroDir !== '' && !id.startsWith(astroDir)) {
+		async transform(inputCode = "", id = "") {
+			if (astroDir !== "" && !id.startsWith(astroDir)) {
 				return null;
 			}
 			return plugin.transform(inputCode, id);
 		},
 	};
 	console.log(plugin);
-}
+};
